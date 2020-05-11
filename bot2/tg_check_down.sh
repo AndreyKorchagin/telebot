dd5c065e2c5e5499106d978a0a9228563a8220d7
AndreyKorchagin <root@OpenWrt.lan>
2020-05-11 12:11:57 +0300

    up

--- bot2/tg_check_down.sh
+++ bot2/tg_check_down.sh
@@ -1,19 +1,34 @@
 #!/bin/sh
-py=$(ps | grep "python /root/telebot/bot2/bot2.py" | head -n 1 | awk '{print $5}')
-path=$(ps | grep "python /root/telebot/bot2/bot2.py" | head -n 1 | awk '{print $6}')
 
-status=0
+count=$(ps | grep "python /root/telebot/bot2/bot2.py" | wc -l)
 
-if [[ $py = "python" ]]
+if [[ $count = 1 ]]
 then
-	if [[ $path = "/root/telebot/bot2/bot2.py" ]]
-	then
-		status=1
+	echo "True $count"
+	py=$(ps | grep "python /root/telebot/bot2/bot2.py" | head -n 1 | awk '{print $5}')
+	path=$(ps | grep "python /root/telebot/bot2/bot2.py" | head -n 1 | awk '{print $6}')
+	status=0
+	if [[ $py = "python" ]]
+	then        
+		if [[ $path = "/root/telebot/bot2/bot2.py" ]]       
+		then                
+			status=1        
+		fi
 	fi
-fi
+	
+	if [[ $status = 0 ]]
+	then        
+		sh /root/telebot/bot2/start &        
+		echo -e "bot restart \t`date`" >> /root/telebot/bot2/log_down
+	fi
 
-if [[ $status = 0 ]]
+elif [[ $count -gt 2 ]]
 then
-	sh /root/telebot/bot2/start &
-	echo "bot restart `date`" >> /root/telebot/bot2/log_down
+	while [ $count -gt 2 ]
+	do
+		pid=$(ps | grep "python /root/telebot/bot2/bot2.py" | tail -2 | head -1 | awk '{print $1}')
+		kill $pid
+		count=$(ps | grep "python /root/telebot/bot2/bot2.py" | wc -l)
+		echo -e "bot kill $pid \t`date`" >> /root/telebot/bot2/log_down
+	done
 fi
