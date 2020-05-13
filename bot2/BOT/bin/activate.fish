9981679e929ea652af77b9a2909907319d3da6a9
root <root@KORCH.lan>
2020-05-14 00:52:09 +0300

    add venv

--- bot2/BOT/bin/activate.fish
+++ bot2/BOT/bin/activate.fish
@@ -57,7 +57,7 @@ end
 # Unset irrelevant variables.
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV '/Users/andreykorchagin/Desktop/Git/telebot/bot2/BOT'
+set -gx VIRTUAL_ENV '/root/telebot/bot2/BOT'
 
 # https://github.com/fish-shell/fish-shell/issues/436 altered PATH handling
 if test (echo $FISH_VERSION | head -c 1) -lt 3
