a25262907921fb1448fbae6952ebef2bf11b63f1
root <root@KORCH.lan>
2020-12-27 05:01:41 +0300

    add

--- bot2/on_off.py
+++ bot2/on_off.py
@@ -1,6 +1,7 @@
 import sys
 import os
 from array import array
+import edit_firewall as ef
 
 # mode = 0 - check status
 # mode = 1 - reverse status
@@ -31,15 +32,32 @@ def check(mode):
 				block = i + 1
 				status = 1
 			else:
-				block = i + 1;
-				status = 0;
-
+				block = i + 1
+				status = 0
 
 	if mode == 1:
 		if status:
 			content.pop(block)
+
+			f = open("/root/test", "a")
+			for item in ef.getListMacsFromFirewall("option name 'Gera'\n"):
+				print("ubus call hostapd.wlan1 del_client \"{\'addr\':'%s', \'reason\':5, \'deauth\':false, \'ban_time\':100000}\"" % item.lower())
+				
+				firewall = f.write("ubus call hostapd.wlan1 del_client \"{\'addr\':'%s', \'reason\':5, \'deauth\':false, \'ban_time\':100000}\"\n" % item.lower())
+				os.system("ubus call hostapd.wlan1 del_client \"{\'addr\':'%s', \'reason\':5, \'deauth\':false, \'ban_time\':100000}\"" % item.lower())
+			f.close()
 		else:
 			content.insert(block, str2[0])
+
+			# f = open("/root/test", "a")
+			# for item in ef.getListMacsFromFirewall("option name 'Gera'\n"):
+			# 	print("ubus call hostapd.wlan1 del_client \"{\'addr\':'%s', \'reason\':5, \'deauth\':false, \'ban_time\':100000}\"" % item.lower())
+				
+			# 	firewall = f.write("ubus call hostapd.wlan1 del_client \"{\'addr\':'%s', \'reason\':5, \'deauth\':false, \'ban_time\':100000}\"\n" % item.lower())
+			# 	os.system("ubus call hostapd.wlan1 del_client \"{\'addr\':'%s', \'reason\':5, \'deauth\':false, \'ban_time\':100000}\"" % item.lower())
+			# f.close()
+
+
 
 		f = open(file, "w")
 		content = "".join(content)
